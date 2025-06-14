6c531bed9386ee6077e394511bad7f53f131f303
nghia <donghia1509@gmail.com>
2025-06-14 18:15:03 +0700

    update sign

--- admin/views/order/updatebill.php
+++ admin/views/order/updatebill.php
@@ -1,1 +1,68 @@
-test
+<?php
+require_once 'layout/header.php';
+require_once 'layout/navbar.php';
+?>
+<!-- Content Wrapper -->
+<div id="content-wrapper" class="d-flex flex-column">
+
+    <!-- Main Content -->
+    <div id="content">
+
+        <!-- Topbar -->
+        <?php require_once 'layout/topbar.php' ?>
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="d-sm-flex align-items-center justify-content-between mb-4">
+                <h1 class="h3 mb-0 text-gray-800">Chi tiết đơn hàng</h1>
+            </div>
+
+            <!-- Content Row -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3">
+                    <h6 class="m-0 font-weight-bold text-primary">Thông tin sản phẩm trong đơn hàng</h6>
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-bordered" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>Ảnh</th>
+                                    <th>Tên sản phẩm</th>
+                                    <th>Màu sắc</th>
+                                    <th>Giá</th>
+                                    <th>Số lượng</th>
+                                    <th>Tổng tiền</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                $total = 0;
+                                foreach ($oneBill as $index => $item):
+                                    $itemTotal = $item['price'] * $item['quantity'];
+                                    $total += $itemTotal;
+                                    ?>
+                                    <tr>
+                                        <td><img src="../assets/img/<?= $item['img_product'] ?>" alt="" width="100px"></td>
+                                        <td><?= $item['name_product'] ?></td>
+                                        <td><?= $item['name_color'] ?></td>
+                                        <td><?= number_format($item['price']) ?>đ</td>
+                                        <td><?= $item['quantity'] ?></td>
+                                        <td><?= number_format($item['price'] * $item['quantity']) ?>đ</td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th colspan="5" class="text-left">Tổng tiền:</th>
+                                    <th><?= number_format($total) ?>đ</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+            
+            
